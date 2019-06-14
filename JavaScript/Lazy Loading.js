510df5173bcfaeb7ca1d11035bbaa5122f940ccd
carmenio <carmenio12@outlook.com>
2019-06-14 15:56:18 +1000

    lazy loading on index

---
+++ JavaScript/Lazy Loading.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.Images"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("Images");
+          lazyImageObserver.unobserve(lazyImage);
+		  lazyImage.classList.add("LazyImages");
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Possibly fall back to a more compatible method here
+  }
+});
