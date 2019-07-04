e2dad528b0fdba93538011fe9181f875024a32ff
carmenio <carmenio12@outlook.com>
2019-07-04 14:26:30 +1000

    Update Photo Upload All - Photography.js

--- JavaScript/Photo Upload All - Photography.js
+++ JavaScript/Photo Upload All - Photography.js
@@ -2,21 +2,21 @@
 $(document).ready( function() {
  
  $("span-beach").each( function(i) {
-  $(this).append("<img src='Images/Sorted/Beach/Insta Photo-"+(++i)+".JPG' />");	
+  $(this).append("<img src='https://github.com/carmenio/carmenio.github.io/blob/master/Images/Sorted/Beach/Insta%20Photo-"+(++i)+".jpg' />");	
  });
  
 });
 $(document).ready( function() {
  
  $("span-water").each( function(i) {
-  $(this).append("<img src='Images/Sorted/Water/Insta Photo-"+(++i)+".JPG' />");	
+  $(this).append("<img src='https://github.com/carmenio/carmenio.github.io/blob/master/Images/Sorted/Water/Insta%20Photo-"+(++i)+".jpg' />");	
  });
  
 });
 $(document).ready( function() {
  
  $("span-people").each( function(i) {
-  $(this).append("<img src='Images/Sorted/People/Insta Photo-"+(++i)+".JPG' />");	
+  $(this).append("<img src='https://github.com/carmenio/carmenio.github.io/blob/master/Images/Sorted/People/Insta%20Photo-"+(++i)+".jpg' />");	
  });
  
 });
